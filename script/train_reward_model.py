695fe11b6c1758edf0f6e8cc3adaf46092826912
Charin Polpanumas <charin.job@gmail.com>
2023-03-06 14:50:00 +0000

    explicitly separate min and max learning rate

--- script/train_reward_model.py
+++ script/train_reward_model.py
@@ -38,12 +38,13 @@ class ScriptArguments:
     per_device_eval_batch_size: Optional[int] = field(default=8)
     gradient_accumulation_steps: Optional[int] = field(default=2)
     #lr stuff
-    learning_rate: Optional[float] = field(default=2e-5)
+    max_learning_rate: Optional[float] = field(default=2e-5)
+    min_learning_rate: Optional[float] = field(default=0.)
     weight_decay: Optional[float] = field(default=0.001)
     warmup_ratio: Optional[float] = field(default=0.1)
     #logging stuff
     wandb_project: Optional[str] = field(default="php_reward_model")
-    logging_steps: Optional[int] = field(default=1)
+    logging_steps: Optional[int] = field(default=50)
     #eval stuff
     eval_steps: Optional[int] = field(default=1000)
     #model and dataset
@@ -71,7 +72,7 @@ ds = load_dataset(script_args.dataset_name)
 # Define the training args. Needs to be done before the model is loaded if you are using deepspeed.
 training_args = TrainingArguments(
     output_dir=f"{script_args.model_name}_reward_model",
-    learning_rate=script_args.learning_rate,
+    learning_rate=script_args.max_learning_rate,
     per_device_train_batch_size=script_args.per_device_train_batch_size,
     per_device_eval_batch_size=script_args.per_device_eval_batch_size,
     num_train_epochs=script_args.num_train_epochs,
@@ -180,14 +181,14 @@ class RewardTrainer(Trainer):
     
     def create_optimizer_and_scheduler(self, num_training_steps: int):
         params = self.get_model().parameters()
-        optimizer = AdamW(params, lr=self.args.learning_rate, 
+        optimizer = AdamW(params, lr=self.args.max_learning_rate, 
                           weight_decay=self.args.weight_decay,
                           bias_correction=True)
         total_steps = num_training_steps
         warmup_steps = int(self.args.warmup_ratio*total_steps)
         scheduler = WarmupDecayLR(optimizer, total_num_steps=total_steps,
-                                  warmup_min_lr=0.,
-                                  warmup_max_lr=self.args.learning_rate,
+                                  warmup_min_lr=script_args.min_learning_rate,
+                                  warmup_max_lr=script_args.max_learning_rate,
                                   warmup_num_steps=warmup_steps,)
         return optimizer, scheduler
 
