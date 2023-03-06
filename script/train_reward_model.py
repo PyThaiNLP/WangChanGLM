f64e189c710991ca844ac6da5f434b50a054abc2
Charin Polpanumas <charin.job@gmail.com>
2023-03-06 11:22:19 +0000

    add reward model trainign on xlm-roberta-base and 8 GPUs 16GB

---
+++ script/train_reward_model.py
@@ -0,0 +1,198 @@
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    TrainingArguments,
+    Trainer,
+    PreTrainedTokenizerBase,
+    HfArgumentParser,
+)
+from transformers.utils import PaddingStrategy
+from typing import Optional, Union, List, Dict, Any
+import evaluate
+from dataclasses import dataclass, field
+import torch.nn as nn
+import numpy as np
+import wandb
+import multiprocessing
+cpu_cores = multiprocessing.cpu_count()
+
+# python -m torch.distributed.launch --nproc_per_node=8 train_reward_model.py \
+# --per_device_train_batch_size=8 --per_device_eval_batch_size=8 --gradient_accumulation_steps=2 \
+# --model_name=xlm-roberta-base --bf16 --deepspeed=../config/reward_model_deepspeed_config.json
+
+# Define and parse arguments.
+@dataclass
+class ScriptArguments:
+    """
+    These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
+    """
+    num_train_epochs: Optional[int] = field(default="2")
+    resume_from_checkpoint: Optional[bool] = field(default=False)
+    #multigpu stuff
+    local_rank: Optional[int] = field(default=0)
+    deepspeed: Optional[str] = field(default=None)
+    per_device_train_batch_size: Optional[int] = field(default=8)
+    per_device_eval_batch_size: Optional[int] = field(default=8)
+    gradient_accumulation_steps: Optional[int] = field(default=2)
+    #lr stuff
+    learning_rate: Optional[int] = field(default=3e-4)
+    weight_decay: Optional[int] = field(default=0.001)
+    lr_scheduler_type: Optional[str] = field(default="cosine")
+    warmup_ratio: Optional[int] = field(default=0.1)
+    #logging stuff
+    wandb_project: Optional[str] = field(default="php_reward_model")
+    logging_steps: Optional[int] = field(default=100)
+    #eval stuff
+    eval_steps: Optional[int] = field(default=1000)
+    #model and dataset
+    model_name: Optional[str] = field(default="xlm-roberta-base")
+    dataset_name: Optional[str] = field(default="pythainlp/php_reward")
+    better_column: Optional[str] = field(default="human_ref_1st")
+    worse_column: Optional[str] = field(default="human_ref_2nd")
+    train_split_name: Optional[str] = field(default="train") 
+    eval_split_name: Optional[str] = field(default="test") 
+    #tokenizer stuff
+    max_length: Optional[int] = field(default=512)
+    #half precision stuff
+    bf16: Optional[bool] = field(default=True,)
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
+
+# initialize wandb with project and run names
+wandb.init(project=script_args.wandb_project, 
+           name=f"{script_args.wandb_project}_{wandb.util.generate_id()}")
+
+# Load the human comparisons dataset for tuning the reward model.
+ds = load_dataset(script_args.dataset_name)
+
+# Define the training args. Needs to be done before the model is loaded if you are using deepspeed.
+training_args = TrainingArguments(
+    output_dir=f"{script_args.model_name}_reward_model",
+    learning_rate=script_args.learning_rate,
+    per_device_train_batch_size=script_args.per_device_train_batch_size,
+    per_device_eval_batch_size=script_args.per_device_eval_batch_size,
+    num_train_epochs=script_args.num_train_epochs,
+    weight_decay=script_args.weight_decay,
+    lr_scheduler_type=script_args.lr_scheduler_type,
+    warmup_ratio=script_args.warmup_ratio,
+    evaluation_strategy="steps",
+    eval_steps=script_args.eval_steps,
+    logging_steps=script_args.logging_steps,
+    save_strategy="epoch",
+    gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+    deepspeed=script_args.deepspeed,
+    local_rank=script_args.local_rank,
+    remove_unused_columns=False,
+    label_names=[],
+)
+
+# Load the value-head model and tokenizer.
+tokenizer = AutoTokenizer.from_pretrained(script_args.model_name)
+model = AutoModelForSequenceClassification.from_pretrained(script_args.model_name, num_labels=1)
+
+# Need to do this for gpt2, because it doesn't have an official pad token.
+tokenizer.pad_token = tokenizer.eos_token
+model.config.pad_token_id = tokenizer.eos_token_id
+original_columns = ds[script_args.train_split_name].column_names
+columns_to_remove = [i for i in original_columns if i not in [script_args.better_column,
+                                                             script_args.worse_column]]
+
+# Tokenize the dataset.
+def preprocess_function(examples):
+    tokenized_j = tokenizer(examples[script_args.better_column], 
+                            truncation=True, max_length=args.max_length)
+    tokenized_k = tokenizer(examples[script_args.worse_column], 
+                            truncation=True, max_length=args.max_length)
+    return {
+        "input_ids_j": tokenized_j["input_ids"],
+        "attention_mask_j": tokenized_j["attention_mask"],
+        "input_ids_k": tokenized_k["input_ids"],
+        "attention_mask_k": tokenized_k["attention_mask"],
+    }
+
+tokenized_ds = ds.map(preprocess_function, batched=True, 
+                      num_proc=cpu_cores, 
+                      remove_columns=columns_to_remove)
+
+# We need to define a special data collator that batches the data in our j vs k format.
+@dataclass
+class RewardDataCollatorWithPadding:
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = script_args.max_length
+    pad_to_multiple_of: Optional[int] = None
+    return_tensors: str = "pt"
+
+    def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
+        features_j = []
+        features_k = []
+        for feature in features:
+            features_j.append({"input_ids": feature["input_ids_j"], 
+                               "attention_mask": feature["attention_mask_j"]})
+            features_k.append({"input_ids": feature["input_ids_k"], 
+                               "attention_mask": feature["attention_mask_k"]})
+        batch_j = self.tokenizer.pad(
+            features_j,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=self.return_tensors,
+        )
+        batch_k = self.tokenizer.pad(
+            features_k,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=self.return_tensors,
+        )
+        batch = {
+            "input_ids_j": batch_j["input_ids"],
+            "attention_mask_j": batch_j["attention_mask"],
+            "input_ids_k": batch_k["input_ids"],
+            "attention_mask_k": batch_k["attention_mask"],
+            "return_loss": True,
+        }
+        return batch
+
+
+# Define the metric that we'll use for validation.
+accuracy = evaluate.load("accuracy")
+
+def compute_metrics(eval_pred):
+    predictions, _ = eval_pred
+    # Here, predictions is rewards_j and rewards_k.
+    # We want to see how much of the time rewards_j > rewards_k.
+    predictions = np.argmax(predictions, axis=0)
+    labels = np.zeros(predictions.shape)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+class RewardTrainer(Trainer):
+    # Define how to compute the reward loss.
+    def compute_loss(self, model, inputs, return_outputs=False):
+        rewards_j = model(input_ids=inputs["input_ids_j"], 
+                          attention_mask=inputs["attention_mask_j"])[0]
+        rewards_k = model(input_ids=inputs["input_ids_k"], 
+                          attention_mask=inputs["attention_mask_k"])[0]
+        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
+        if return_outputs:
+            return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
+        return loss
+
+# Train the model, woohoo.
+trainer = RewardTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_ds[script_args.train_split_name],
+    eval_dataset=tokenized_ds[script_args.eval_split_name],
+    compute_metrics=compute_metrics,
+    data_collator=RewardDataCollatorWithPadding(tokenizer=tokenizer),
+)
+
+trainer.train(script_args.resume_from_checkpoint)
+
+# Push to the hub so you can share it with people :D
+model.push_to_hub(script_args.model_name)
+tokenizer.push_to_hub(script_args.model_name)
