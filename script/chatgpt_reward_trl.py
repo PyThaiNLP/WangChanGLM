dca6ecef0b1eb39e190f947a3aa5795da5dd9d91
Mr.P L <pingloaf@gmail.com>
2023-11-22 08:43:29 +0700

    remove token

--- script/chatgpt_reward_trl.py
+++ script/chatgpt_reward_trl.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# !huggingface-cli login --token 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
-
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
